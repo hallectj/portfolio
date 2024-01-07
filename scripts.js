ec1a2f512afc9fd2d1931f6112c5e23b8dcf69b3
Travis Halleck <hallectj@gmail.com>
2024-01-06 21:56:02 -0600

    added a project to portfolio page

--- scripts.js
+++ scripts.js
@@ -8,16 +8,19 @@ function changeAvator(){
   
   let myPicImage = document.getElementsByClassName('myPic')[0];
 
-  $('.avator_pic').find('img').on('mouseover', function(){
-    const picId = $(this).attr('data-picnum');
-    if($(this).is(":hover")){
-      const obj = picArray.find(v => v.pic_id === picId);
-      myPicImage.setAttribute("src", obj.image);
-    }
+  document.querySelectorAll('.avator_pic img').forEach(function(imgElement) {
+    imgElement.addEventListener('mouseover', function() {
+        const picId = imgElement.getAttribute('data-picnum');
+        if (imgElement.matches(':hover')) {
+            const obj = picArray.find(function(v) {
+                return v.pic_id === picId;
+            });
+            myPicImage.setAttribute('src', obj.image);
+        }
+    });
   });
-
 }
 
-$(document).ready(function() {
-  changeAvator()
-});
+document.addEventListener("DOMContentLoaded", () => {
+  changeAvator();
+}, false);
